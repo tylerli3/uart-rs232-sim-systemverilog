0e568c98398965c329aa9944d75e6f63187beb48
tylerli3 <tylerli3@illinois.edu>
2025-08-18 22:44:02 -0500

    Add files via upload

--- src/baud_gen/baud_gen.sv
+++ src/baud_gen/baud_gen.sv
@@ -1,1 +1,23 @@
-
+module baud_gen #(parameter DIVIDER = 10) // set div param for timing, needs to be >1
+( // io
+	input logic clk, // external clk
+	input logic rst_n, // reset signal (inverted)
+	output logic tick // baud tick out
+);
+	logic [$clog2(DIVIDER)-1:0] counter; // counter for tick out
+	
+	always_ff @(posedge clk or negedge rst_n) begin // start logic (async reset)
+		if (!rst_n) begin // if reset on (rst_n = 0), no ticks
+			counter <= 0;
+			tick <=0; 
+		end else begin // if counter reached div, out a tick
+			if (counter == DIVIDER-1) begin // reached threshold, out tick
+				counter <= 0;
+				tick <= 1;
+			end else begin // not time to tick yet
+				counter <= counter+1;
+				tick <= 0;
+			end
+		end
+	end
+endmodule
--- src/baud_gen/tb_baud_gen.sv
+++ src/baud_gen/tb_baud_gen.sv
@@ -1,1 +1,56 @@
+`timescale 1ns/1ps
 
+module tb_baud_gen;
+
+    // params
+    localparam DIVIDER = 10;     // tick every 10 clock cycles
+    localparam CLK_PERIOD = 10;  // clock = 100 MHz
+
+    // tb io
+    logic clk;
+    logic rst_n;
+    logic tick;
+
+    // init dut
+    baud_gen #(.DIVIDER(DIVIDER)) dut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .tick(tick)
+    );
+
+    // clk gen
+    initial begin
+        clk = 0;
+        forever #(CLK_PERIOD/2) clk = ~clk;
+    end
+
+    initial begin
+        // init rst signal
+        rst_n = 0;
+
+        // hold reset first
+        #(CLK_PERIOD*20);
+        rst_n = 1;
+
+        // let baud run
+        #(CLK_PERIOD*105);
+
+		  // reset at async interval
+		  rst_n = 0;
+        #(CLK_PERIOD*25); 
+		  
+		  //test baud again from rst
+		  rst_n = 1;
+        #(CLK_PERIOD*100);
+		 
+        // End simulation
+        $finish;
+    end
+
+    // Monitor output
+    initial begin
+        $display("Time\tTick");
+        $monitor("%0t\t%b", $time, tick);
+    end
+
+endmodule
